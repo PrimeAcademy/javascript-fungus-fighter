583fde67e702f1ec80c4b6883b85a18243540cd3
Sean Harrison <sharrison9@gmail.com>
2023-12-06 12:13:33 -0600

    made array for attacks

--- script.js
+++ script.js
@@ -4,15 +4,33 @@
 // let fungusHP = 100;
 
 let playerAP = 100
-let fungusHP = 100
+let monsterHP = 100
 
-// make array for attacks
+// make array for attacks, will 
+// eventually be accessed by buttons in HTML
 
 let attacks = [
-
-
-    
-]
+    {
+        name: 'arcaneScepter',
+        apCost: 12,
+        hpDamage: 14
+    },
+    {
+        name: 'entangle',
+        apCost: 23,
+        hpDamage: 9
+    },
+    {
+        name: 'dragonBlade',
+        apCost: 38,
+        hpDamage: 47
+    },
+    {
+        name: 'starFire',
+        apCost: 33,
+        hpDamage: 25
+    }
+];
 
 function onReady() {
     console.log("Ready to go!")
